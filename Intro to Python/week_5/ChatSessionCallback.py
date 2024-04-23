84901aa01efd0add7de4eed3e363e6f0ea0ed0ca
Johnny <JohnnyLieu@yahoo.com>
2024-04-22 20:17:02 -0700

    week five final project

---
+++ Intro to Python/week_5/ChatSessionCallback.py
@@ -0,0 +1,73 @@
+"""This final project is designed to incorporate all the tools learned throughout the course. 
+You will need to create a class named ChatSesssion where it will have a constructor that will take in a parameter called filename. 
+The constructor will create include the following variables:
+
+        self.numLines = 0
+        self.TagsList = []
+        self.MembersList = []
+        self.TimesList = []
+        self.UniqueMembersList = []
+        self.UniqueTagsList = []
+        self.MessageList = []
+
+It should open the file and begin reading each line and populating the variables above in a for loop
+
+        f = open(filename, "r")
+        records = f.readlines()
+
+        for record in records:
+
+                      ............................
+
+The file consists of Tags, Timestamps, Members, Users, and Messages. 
+Because there are multiple members you will need to create a Unique members list that hold the name of the unique members as well as unique tags. 
+The class should contain the following tags that will return the information above:
+
+    GetNumLines(self):
+    GetTagsList(self):
+    GetTimesList(self):
+    GetMembersList(self):
+    GetUserSysList(self):
+    GetUniqueMembersList(self):
+    GetUniqueTagsList(self):
+    GetMessageList(self):
+
+Create an object to test out all the information above as follows:  
+
+sessionObj = ChatSessions('iphone_07_18-1.annot')
+
+** Note: The file data is of the type:
+
+T-1 20816 Eryn * entered the room.
+
+where
+
+T-1 is the tag
+20816 is the timestamp
+Eryn is the member
+*  (asterisk) is the UserSys symbol
+entered the room. is the message
+Upload the Python Files so that I may be able to test out your code. Do not simply upload an image of the code.
+
+Good luck and enjoy!"""
+
+from ClassSessions import ChatSessions
+
+sessionObj = ChatSessions('iphone_07_18-1.annot')
+num = sessionObj.GetNumLines()
+tags = sessionObj.GetTagsList()
+times = sessionObj.GetTimesList()
+members = sessionObj.GetMembersList()
+userSys = sessionObj.GetUserSysList()
+uniqueMembers = sessionObj.GetUniqueMembersList()
+uniqueTags = sessionObj.GetUniqueTagsList()
+messages = sessionObj.GetMessageList()
+
+# print(num)
+# print(tags)
+# print(times)
+print(members)
+# print(userSys)
+# print(uniqueTags)
+# print(uniqueMembers)
+# print(messages)
---
+++ Intro to Python/week_5/ClassSessions.py
@@ -0,0 +1,118 @@
+import re
+
+class ChatSessions:
+    numLines = 0
+    TagsList = []
+    MembersList = []
+    UserSysList = []
+    TimesList = []
+    UniqueMembersList = []
+    UniqueTagsList = []
+    MessageList = []
+
+    def __init__(self, file):
+        self.file = file
+
+    def GetNumLines(self):
+        with open(self.file, 'r') as file:
+            for line in file:
+                self.numLines += 1
+        
+        return self.numLines
+    
+    def GetTagsList(self):
+        regex = r'^T-?\d+ '
+        """
+        ^ asserts the start of the line.
+        T-? matches the letter "T" followed by an optional hyphen.
+        \d+ matches one or more digits.
+        ' ' matches a space character, indicating the end of the series of numbers.
+        """
+
+        with open(self.file, 'r') as file:
+            for line in file:
+                match = re.match(regex, line)
+                if match:
+                    tag = match.group(0)  # Extract the matched portion
+                    self.TagsList.append(tag)
+        
+        return self.TagsList
+    
+    def GetTimesList(self):
+        regex = r'T-?\d+\s(\d+)'
+
+        with open(self.file, 'r') as file:
+            for line in file:
+                matches = re.findall(regex, line)
+                if matches:
+                    self.TimesList.extend(matches)
+        
+        return self.TimesList
+
+    def GetMembersList(self):
+        regex = r'T-\d+\s(\w+)\s\*'
+        """
+        this matches the series number "T-1", 
+        followed by a space, 
+        then captures the name using the parentheses \w+, 
+        and finally matches a space followed by an asterisk symbol. 
+        """
+        with open(self.file, 'r') as file:
+            for line in file:
+                if re.match(regex, line):
+                    self.MembersList.append(line)
+
+        return self.MembersList
+
+
+    def GetUserSysList(self):
+        regex = r'T-\d+\s(\w+)\s([*:])\s'
+        """
+        matches the series number "T-1", 
+        followed by a space, captures the name, 
+        then matches either a * or : character, 
+        followed by a space.
+        """
+
+        with open(self.file, 'r') as file:
+            for line in self.file:
+                if re.match(regex, line):
+                    self.UserSysList.append(line)
+        
+        return self.UserSysList
+
+    
+    def GetUniqueMembersList(self):
+        regex = r'T-\d+\s(\w+)\s\*'
+
+        with open(self.file, 'r') as file:
+            for line in self.file:
+                if re.match(regex, line) and line == re.match.group(1):
+                    self.UniqueMembersList.append(line)
+
+        return self.UniqueMembersList
+    
+    def GetUniqueTagsList(self):
+        regex = r'^T-?\d*$'
+
+        with open(self.file, 'r') as file:
+            for line in self.file:
+                if re.match(regex, line) and line == re.match.group(1):
+                    self.UniqueTagsList.append(line)
+        
+        return self.UniqueTagsList
+    
+    def GetMessageList(self):
+        regex = r'T-\d+\s\w+\s\* (.*)'
+        """
+        T-\d+: This part matches "T-" followed by one or more digits.
+        \s: This matches a whitespace character.
+        \w+: This matches one or more word characters (letters, digits, or underscores) representing the name.
+        \s\*: This matches a space followed by an asterisk symbol *.
+        (.*): This part captures any characters (except newline characters) after the asterisk symbol *
+        """
+
+        with open(self.file, 'r') as file:
+            for line in self.file:
+                if re.match(regex, line):
+                    self.MessageList(line)
