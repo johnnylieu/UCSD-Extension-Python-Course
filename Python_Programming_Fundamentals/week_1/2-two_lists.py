959cddc9bd6e647a32434722a1b48fe7222b4e71
Johnny <JohnnyLieu@yahoo.com>
2024-05-12 20:08:08 -0700

    week 1 part 2 done

---
+++ Python_Programming_Fundamentals/week_1/2-two_lists.py
@@ -0,0 +1,17 @@
+"""Take two lists, say for example these two:
+
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes."""
+
+def common_elements(a, b):
+    common = set(a).intersection(b)
+    common = list(common)
+    return(f"Lists {a} & {b} have these elements in common: {common}.")
+
+
+if __name__ == "__main__":
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    print(common_elements(a, b))
