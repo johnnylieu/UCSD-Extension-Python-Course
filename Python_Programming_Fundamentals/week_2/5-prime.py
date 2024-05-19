d5c8e822cd1509e141828737325f49f6fdae874f
Johnny <JohnnyLieu@yahoo.com>
2024-05-18 22:15:13 -0700

    assignment 5 done

--- Python_Programming_Fundamentals/week_2/5-prime.py
+++ Python_Programming_Fundamentals/week_2/5-prime.py
@@ -1,2 +1,25 @@
 """Write a function or set of functions that asks a user for a number and determine whether the number is prime or not. 
 Recall that a prime number is a number that has no divisors."""
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+if __name__ == "__main__":
+    user_input = abs(int(input("Enter a positive whole number: ")))
+    is_prime(user_input)
+
+    if is_prime(user_input):
+        print(f"{user_input} is a prime number.")
+    else:
+        print(f"{user_input} is not a prime number.")
