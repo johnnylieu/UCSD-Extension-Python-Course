d8a92a86a0f7a86055c21ade0b3e0ce03c147f9b
Johnny <JohnnyLieu@yahoo.com>
2024-05-18 21:50:37 -0700

    assignment 3 hearts and spades done, this was a difficult one but very fun

---
+++ Python_Programming_Fundamentals/week_2/4-finbonacci_sequence.py
@@ -0,0 +1,7 @@
+"""Fibonacci Sequence is the series of positive numbers that is created by add the previous two numbers to create the next number in a sequence. 
+For example, A Fibonacci sequence starts with the number 1. 
+Then next number adds the two that number plus the previous number which happens to be 0+1 so the sequence would now be 0+1 = 1 giving us the sequence 1, 1, 
+The next number is created by add these two 1+1 = 2 giving us 1,1,2 
+and so on until we have 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, ...
+Write a recursive function that asks the user how many Fibonacci numbers to generate and then generates them."""
+
---
+++ Python_Programming_Fundamentals/week_2/5-prime.py
@@ -0,0 +1,2 @@
+"""Write a function or set of functions that asks a user for a number and determine whether the number is prime or not. 
+Recall that a prime number is a number that has no divisors."""
