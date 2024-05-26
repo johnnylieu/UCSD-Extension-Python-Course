d3a9a87e6a013be617cc18301f0c417a1d47f2b2
Johnny <JohnnyLieu@yahoo.com>
2024-05-26 13:28:28 -0700

    assignment 3 done

--- Python_Programming_Fundamentals/week_3/3-unnitesting.py
+++ Python_Programming_Fundamentals/week_3/3-unnitesting.py
@@ -28,3 +28,24 @@ In your unittest you may want to try various options such as using two positive 
 
 import calc
 import unittest
+
+class Test_Calc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(3, 5), 8)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(3, 5), 15)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+
+        # nothing can be divded by zero
+        with self.assertRaises(ValueError):
+            calc.divide(1, 0)
+
+if __name__ == "__main__":
+    unittest.main()
