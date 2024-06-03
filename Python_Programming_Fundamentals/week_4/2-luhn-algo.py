106da2ecf38eef71c7b8782989dc9dcca61cb6ce
Johnny <JohnnyLieu@yahoo.com>
2024-06-02 20:46:34 -0700

    i forgot to complete the rest of assignment 2, it is not complete

--- Python_Programming_Fundamentals/week_4/2-luhn-algo.py
+++ Python_Programming_Fundamentals/week_4/2-luhn-algo.py
@@ -22,6 +22,28 @@ pattern = r"\bVisa #\s*(\d+)\b"
 match = re.search(pattern, text)
 
 if __name__ == "__main__":
-    print(match.group(1))
+    print(f"The credit card number is: {match.group(1)}")
     # question why does printing match alone return Visa # in front of the cc number?
     # print(match)
+
+    # converting to list so i can use lambda function
+    nums_list = list(match.group(1))
+    # converts every value to an integer
+    nums_list = [int(x) for x in nums_list]
+    # i had to add 1 to the index because indexing started at 0 instead of 1
+    doubled_nums_list = list(map(lambda x : int(x) *2 if (nums_list.index(x) + 1) % 2 == 0 else x, nums_list))
+    print(f"Doubled is {doubled_nums_list}")
+
+    # If double of a digit is more then 9, then subtract 9 to that number
+    double_digits = list(map(lambda x: int(x)-9 if int(x)>9 else x, doubled_nums_list))
+    print(f"If double of a digit is more then 9, then subtract 9 to that number: {double_digits}")
+
+    # Sum all the numbers together and find the modulus (%10). If the modulus is equal to zero, the number is valid
+    sum_of_nums_list = sum(nums_list)
+    sum_of_doubled_nums_list = sum(doubled_nums_list)
+    sum_of_double_digits = sum(double_digits)
+    print(
+    f"The sum of original with modulus 10 is: {sum_of_nums_list % 10}.\n"
+    f"The sum of doubled with modulus 10 list is: {sum_of_doubled_nums_list % 10}.\n"
+    f"The sum of double digits with modulus 10 is: {sum_of_double_digits % 10}."
+    )
