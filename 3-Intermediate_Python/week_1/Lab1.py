93f54b96f90eb2a82bf27b88b32865be5d325d6e
Johnny <JohnnyLieu@yahoo.com>
2024-07-07 22:54:56 -0700

    changed variable names

--- 3-Intermediate_Python/week_1/Lab1.py
+++ 3-Intermediate_Python/week_1/Lab1.py
@@ -16,16 +16,17 @@ Enter a string: REDIVIDER
 Is 'REDIVIDER' a palindrome? True"""
 
 def isPalindrome():
-    word = input("Enter a string: ")
+    user_string = input("Enter a string: ")
     # the below will tell the user whether or not they entered a word
     # but since you are asking for a string, i'm assuming that includes sentences as well so i removed it
     # if word.isalpha() == False:
     #     print(f"'{word}' is not a word.")
     # the below would have bene an elif if the above was used
-    word_reversed = ''
-    for i in range(len(word)-1, -1, -1):
-        word_reversed += word[i]
-    print(f"Is '{word}' a palindrome? {word == word_reversed}")
+    # user_string would have been word and user_string_reversed would have been word_reversed
+    usedr_string_reversed = ''
+    for i in range(len(user_string)-1, -1, -1):
+        word_reversed += user_string[i]
+    print(f"Is '{user_string}' a palindrome? {user_string == usedr_string_reversed}")
 
 if __name__ == "__main__":
     isPalindrome()
