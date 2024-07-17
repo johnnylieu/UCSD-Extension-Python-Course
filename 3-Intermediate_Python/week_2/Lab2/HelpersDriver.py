84e96caa6e60cea0648e11c0f5b37a2c571932ba
Johnny <JohnnyLieu@yahoo.com>
2024-07-17 07:26:01 -0700

    correct typo

--- 3-Intermediate_Python/week_2/Lab2/HelpersDriver.py
+++ 3-Intermediate_Python/week_2/Lab2/HelpersDriver.py
@@ -1,6 +1,6 @@
 from Helpers import *
 
-if __name__ == "__main++":
+if __name__ == "__main__":
     print(inputInt("Please enter a number: ", 0, 100))
     print(inputFloat("Please enter a number: ", -10, 1000))
     print(inputString("Enter some text: ", 5, 10))
