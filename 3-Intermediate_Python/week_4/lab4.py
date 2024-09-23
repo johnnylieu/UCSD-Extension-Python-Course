cbec2f85a011ba94ce5b7133ff3906e1340bc506
Johnny <JohnnyLieu@yahoo.com>
2024-09-22 18:34:00 -0700

    lab 4, iterator for Leibniz is working

---
+++ 3-Intermediate_Python/week_4/lab4.py
@@ -0,0 +1,44 @@
+from Fraction import *
+from decimal import *
+
+pi50 = Decimal("3.14159265358979323846264338327950288419716939937510")
+iterations = 100
+
+class LeibnizPiIterator:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        self.iterated = 0
+        # running total for the series
+        self.fraction = Fraction(0, 1)
+        # denominator to be used in the next iteration
+        self.n = 1
+        self.add_next = True
+
+        return self
+    
+    def __next__(self):
+        self.next_value = Fraction(4, self.n)
+
+        if self.add_next is True:
+            self.fraction += self.next_value
+        else:
+            self.fraction -= self.next_value
+
+        self.add_next = not self.add_next
+        self.n += 2
+
+        self.iterated += 1
+
+        return self.fraction.value
+    
+    def __str__(self):
+        return f"pi after {self.iterated} iterations: {self.fraction}"
+    
+if __name__ == "__main__":
+    obj = LeibnizPiIterator()
+    obj = iter(obj)
+    for i in range(iterations):
+        next(obj)
+        print(obj)
