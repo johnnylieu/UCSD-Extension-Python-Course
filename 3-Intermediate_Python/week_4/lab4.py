35624da87c16bc44e6effc24ee2508159c77bf4a
Johnny <JohnnyLieu@yahoo.com>
2024-09-22 19:16:03 -0700

    success!

--- 3-Intermediate_Python/week_4/lab4.py
+++ 3-Intermediate_Python/week_4/lab4.py
@@ -40,9 +40,36 @@ class LeibnizPiIterator:
         self.difference = abs(self.decimal_format - pi50)
         return f"pi after {self.iterated} iterations: {self.decimal_format:.50f} \nDifference: {self.difference:.50f}"
     
+
+def NilakanthaPiGenerator():
+    fraction = Fraction(3, 1)
+    num = 2
+    add_next = True
+    iterated = 0
+
+    while True:
+        operand = Fraction(4, num * (num + 1) * (num + 2))
+
+        if add_next:
+            fraction += operand
+        else:
+            fraction -= operand
+
+        num += 2
+        iterated += 1
+
+        yield fraction
+    
 if __name__ == "__main__":
+    ## Leibniz method
     obj = LeibnizPiIterator()
     obj = iter(obj)
     for i in range(iterations):
         next(obj)
     print(obj)
+
+    ## Nilakantha's method
+    obj2 = NilakanthaPiGenerator()
+    for i in range(iterations):
+        approx_pi = next(obj2)
+    print(f"After {iterations} iterations, pi ≈ {approx_pi:.50F}")
